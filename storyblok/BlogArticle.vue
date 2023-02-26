f2ff07ff52aae10a7ba1e13beb19bf530eebd400
Aleks <aleksandar.trpkovski@gmail.com>
2023-02-26 21:15:05 +1100

    Blog Article component has been created.

---
+++ storyblok/BlogArticle.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{
+    blok: {
+        title: string;
+        description: string;
+        image: {
+            filename: string;
+        };
+        first_published_at: string;
+        _uid: string;
+    };
+}>();
+</script>
+
+<template>
+    <div class="bg-white py-24 sm:py-32">
+        <div class="mx-auto container px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+                <p class="text-base font-semibold leading-7 text-blue-600">{{ props.blok.first_published_at }}</p>
+                <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ props.blok.title }}</h1>
+                <div class="mt-10 grid max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
+                    {{ props.blok.description }}
+                </div>
+            </div>
+        </div>
+        <div class="relative overflow-hidden pt-16 lg:pt-20">
+            <div class="mx-auto container px-6 lg:px-8">
+                <img
+                    class="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10 w-full aspect-square"
+                    :src="props.blok.image.filename"
+                    alt="Blog Image"
+                />
+                <div class="relative" aria-hidden="true">
+                    <div class="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -19,10 +19,10 @@ const props = defineProps<{
                     amet fugiat veniam occaecat fugiat aliqua.
                 </p>
                 <div class="mt-10 flex items-center justify-center gap-x-6">
-                    <a
-                        href="#"
+                    <NuxtLink
+                        to="/blog"
                         class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                        >Get started</a
+                        >Get started</NuxtLink
                     >
                     <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
                         >Learn more <span aria-hidden="true">→</span></a
